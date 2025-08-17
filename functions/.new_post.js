5dc913e5f39f2442271722bb5e45325ff324a3b5
est <electronixtar@gmail.com>
2025-08-17 16:14:29 +0800

    js modules sux

--- functions/.new_post.js
+++ functions/.new_post.js
@@ -1,4 +1,9 @@
-import { getCookie } from "../static/utils"
+function getCookie(cookie_str, name) {
+    return decodeURIComponent(cookie_str || ''
+        .split(";")
+        .find(row => row.trim().startsWith(name + "="))
+        ?.split("=")[1]?.trim() || '');
+}
 
 async function handler(request, env) {
     const req_url = new URL(request.url)
