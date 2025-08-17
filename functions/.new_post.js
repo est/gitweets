8304e8cf9c28ef2a642126f0c82a2f5b4a195ef3
est <electronixtar@gmail.com>
2025-08-17 17:52:07 +0800

    哎Object不会

--- functions/.new_post.js
+++ functions/.new_post.js
@@ -6,7 +6,7 @@ function getCookie(cookie_str, name) {
 }
 
 async function fetch_json(url, opts){
-    if (!opts) opts = {}
+    if (!opts || Object.keys(opts).length === 0) opts = {}
     opts.signal ||= AbortSignal.timeout(5000)
     opts.headers ||= {}
     opts.headers['User-Agent'] = 'gitweets/1.0 (cloudflare worker)'
