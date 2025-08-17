55adf523982cd5d33281c1de20c0a18059af4bca
est <electronixtar@gmail.com>
2025-08-17 16:49:11 +0800

    重新搞个 fetch

--- functions/.new_post.js
+++ functions/.new_post.js
@@ -5,6 +5,19 @@ function getCookie(cookie_str, name) {
         ?.split("=")[1]?.trim() || '');
 }
 
+async function fetch_json(url, opts){
+    opts.signal ||= AbortSignal.timeout(5000)
+    const req = fetch(url, opts)
+    rsp = await req.text()
+    try{
+        const obj = JSON.parse(rsp)
+    } catch(err) {
+        console.error(err, rsp)
+        const obj = null
+    }
+    return obj
+}
+
 async function handler(request, env) {
     const req_url = new URL(request.url)
     const repo = req_url.searchParams.get('repo')
@@ -13,33 +26,34 @@ async function handler(request, env) {
     const {message} = await request.json()
     if (!message) return new Response('', {status: 400})
     const API_BASE = `https://api.github.com/repos/${repo}`
-    const r1 = await fetch(`${API_BASE}/commits?per_page=1`, {
+    const r1 = await fetch_json(`${API_BASE}/commits?per_page=1`, {
         method: 'get', signal: AbortSignal.timeout(5000),
     })
-    const r1_rsp = await r1.json()
-    const last_sha = r1_rsp?.[0]?.sha
-    const last_tree = r1_rsp?.[0]?.commit?.tree?.sha
-    if (!last_sha || !last_tree) return new Response('', {status: 400})
-    const r2 = await fetch(`${API_BASE}/commits/${last_sha}/branches-where-head`, {
+    const last_sha = r1?.[0]?.sha
+    const last_tree = r1?.[0]?.commit?.tree?.sha
+    if (!last_sha || !last_tree) return Response.json({error: 'no last commit'}, {status: 400})
+    const r2 = await fetch_json(`${API_BASE}/commits/${last_sha}/branches-where-head`, {
         method: 'get', signal: AbortSignal.timeout(5000),
     })
-    const branch = (await r2.json())?.[0]?.name
+    const branch = r2?.[0]?.name
+    if (!branch) return new Response.json({error: 'no branch'}, {status: 400})
     API_COMMIT_BASE = `${API_BASE}/git/commits`
-    const r3 = await fetch(API_COMMIT_BASE, {
+    const r3 = await fetch_json(API_COMMIT_BASE, {
         method: 'post', header: {"Authorization": `Bearer ${access_token}`},
         signal: AbortSignal.timeout(5000), body: JSON.stringify({
             message: message, tree: last_tree, parents: [last_sha]
         }), credentials: 'include'
     })
     API_HEAD_BASE = `${API_BASE}/git/ref/heads/${branch}`
-    const new_sha = (await r3.json())?.sha
-    const r4 = await fetch(API_HEAD_BASE, {
+    const new_sha = r3?.sha
+    if (!new_sha) return new Response.json({error: 'failed to commit'}, {status: 400})
+    const r4 = await fetch_json(API_HEAD_BASE, {
         method: 'patch', header: {"Authorization": `Bearer ${access_token}`},
         signal: AbortSignal.timeout(5000), body: JSON.stringify({
             sha: new_sha
         }), credentials: 'include'
     })
-    return Response.json(await r4.json(), {status: 201})
+    return Response.json(r4, {status: 201})
 }
 
 export function onRequest(context) {
