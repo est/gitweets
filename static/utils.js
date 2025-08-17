8b9ed4a15ec7d50ccc60d9ae9b9956f6611df48d
est <electronixtar@gmail.com>
2025-08-17 16:14:29 +0800

    js modules sux

--- static/utils.js
+++
@@ -1,6 +0,0 @@
-export function getCookie(cookie_str, name) {
-    return decodeURIComponent(cookie_str || ''
-        .split(";")
-        .find(row => row.trim().startsWith(name + "="))
-        ?.split("=")[1]?.trim() || '');
-}
