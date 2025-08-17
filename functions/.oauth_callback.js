adb7eff95912ded2b83c685ec42ee700139c80d4
est <electronixtar@gmail.com>
2025-08-17 12:01:05 +0800

    payload不会了

--- functions/.oauth_callback.js
+++ functions/.oauth_callback.js
@@ -1,23 +1,21 @@
 async function handler(request, env) {
   const req_url = new URL(request.url)
-  console.log(request.url, req_url)
   const code = (req_url.searchParams.get('code') || '').trim()
-  console.log(code)
   const rspGohome = Response.redirect('/', 302)
   if (code.length != 20 || !env.client_secret || !env.client_id){
     return rspGohome
   }
-  const payload = new URLSearchParams({
+  const payload = {
     "client_id": env.client_id,
     "client_secret": env.client_secret,
     "code": code,
-  })
-  console.log(payload.toString())
+  }
+  console.log(payload)
   const r = await fetch('https://github.com/login/oauth/access_token', {
     method: 'post', headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/x-www-form-urlencoded'},
-    body: payload.toString(),
+    body: new URLSearchParams(payload).toString(),
     signal: AbortSignal.timeout(5000)})
   rsp = await r.json()
   if (rsp.scope == 'public_repo' && rsp.access_token) {
