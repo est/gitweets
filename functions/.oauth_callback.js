84c0dce9f13459292298dee82f07c6c24134c64d
est <electronixtar@gmail.com>
2025-08-17 12:10:49 +0800

    .redirect 居然不允许相对路径

--- functions/.oauth_callback.js
+++ functions/.oauth_callback.js
@@ -1,8 +1,7 @@
 async function handler(request, env) {
   const req_url = new URL(request.url)
   const code = (req_url.searchParams.get('code') || '').trim()
-  const rspGohome = Response.redirect('/', 302)
-  console.log(env)
+  const rspGohome = Response.redirect(req_url.origin, 302)
   if (code.length != 20 || !env.client_secret || !env.client_id){
     return rspGohome
   }
@@ -18,9 +17,9 @@ async function handler(request, env) {
       'Content-Type': 'application/x-www-form-urlencoded'},
     body: new URLSearchParams(payload).toString(),
     signal: AbortSignal.timeout(5000)})
-  rsp = await r.json()
-  if (rsp.scope == 'public_repo' && rsp.access_token) {
-    rspGohome.headers.set('Set-Cookie', `access_token=${rsp.access_token}; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=20000`)
+  res = await r.json()
+  if (res.scope == 'public_repo' && res.access_token) {
+    rspGohome.headers.set('Set-Cookie', `access_token=${res.access_token}; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=20000`)
     return rspGohome
   } else {
     return rspGohome
