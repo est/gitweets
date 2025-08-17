d8fd6a502dc6541bb843bf77c2eb1ead178d710b
est <electronixtar@gmail.com>
2025-08-17 11:27:23 +0800

    增加真正的 oauth 登录

---
+++ functions/.oauth_callback.js
@@ -0,0 +1,30 @@
+async function handler(request, env) {
+  const params = new URLSearchParams(context.request.searchParams)
+  const code = (params.get('code') || '').trim()
+  const rspGohome = Response.redirect('/', 302)
+  if (code.length != 20 || !env.client_secret || env.client_id){
+    return rspGohome
+  }
+  const payload = new URLSearchParams({
+    "client_id": env.client_id,
+    "client_secret": env.client_secret,
+    "code": code,
+  })
+  const r = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'post', headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'},
+    body: payload.toString(),
+    signal: AbortSignal.timeout(5000)})
+  rsp = await r.json()
+  if (rsp.scope == 'public_repo' && rsp.access_token) {
+    rspGohome.headers.set('Set-Cookie', `access_token=${rsp.access_token}; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=20000`)
+    return rspGohome
+  } else {
+    return rspGohome
+  }
+}
+
+export function onRequest(context) {
+    return handler(context.request, context.env)
+}
